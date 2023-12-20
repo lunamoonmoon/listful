0bd7b5830289cf2b9e7a79c07460b32c31dd13af
lunamoonmoon <5862lunamoon@gmail.com>
2023-12-19 17:01:51 -0800

    debugging create table files

--- Backend/src/routes/books.js
+++ Backend/src/routes/books.js
@@ -10,21 +10,27 @@ module.exports = db => {
     //A get request to retreive information about books from a database
     //Table BOOKS (ID, LIBRARY_ID, NAME, AUTHOR, RATING, NOTES, OWNERSHIP)
 
-    db.query(`
-      SELECT 
-      json_agg(
-          json_build_object(
-            'id', books.id,
-            'library_id', books.library_id,
-            'name', books.name,
-            'author', books.author,
-          )
-        ) as book_data
-      FROM books;
-    `).then(({ rows }) => {
-      console.log(rows[0].book_data);
-      response.json(rows[0].book_data);
-    });
+    const getAllBooks = () => {
+      return db
+        .query(`
+          SELECT 
+          json_agg(
+              json_build_object(
+                'id', books.id,
+                'library_id', books.library_id,
+                'name', books.name,
+                'author', books.author,
+              )
+            ) as book_data
+          FROM books;
+          `)
+        .then(({ rows }) => {
+          console.log(rows[0].book_data);
+          response.json(rows[0].book_data);
+        });
+      };
+
+      getAllBooks();
   });
 
   return router;
