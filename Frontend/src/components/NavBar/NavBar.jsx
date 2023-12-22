7e16c7590c9491c01bf14ee292891955b106cdbc
India Cornell <indiaccornell@gmail.com>
2023-12-22 11:45:53 -0800

    Editing Navbar for basic functionality - no css or logo yet

--- Frontend/src/components/NavBar/NavBar.jsx
+++ Frontend/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,54 @@
-import Modal from "../LoginModal/Modal";
+//import Modal from "../LoginModal/Modal";
+//import logo! 
+import React, {useState} from "react";
+import Modal from "../Modal/Modal";
+import About from "../About/About";
 
-export default function NavBar() {
+
+export default function NavBar( {isLoggedIn} ) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+
+  const handleAboutUsClick = () => {
+    setModalContent(<About />);
+    setIsModalOpen(true);
+  };
+
+  const handleSignUpLogInClick = () => {
+    //setModalContent(<SignUpLogIn />);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalContent(null);
+    setIsModalOpen(false);
+  };
 
   return (
     <nav>
-      <div>
-        Logo
-      </div>
-      <div>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <Modal>
-            <button>login/sign up</button>
-            </Modal>
-          </li>
-          <li>My Profile</li>
-          <li>About</li>
-        </ul>
+      <div className="logo">Logo</div>
+      <div className="buttons">
+        {isLoggedIn ? (
+          <>
+            <button>Catalog</button>
+            <button>Search</button>
+          </>
+        ) : (
+          <>
+            <button disabled onClick={() => alert('Please log in to see our collection')}>
+              Catalog
+            </button>
+            <button disabled onClick={() => alert('Please log in to search our collection')}>
+              Search
+            </button>
+          </>
+        )}
+        <button onClick={handleAboutUsClick}>About Us</button>
+        {isModalOpen && <About closeModal={closeModal} />}
+
+        <button onClick={() => handleSignUpLogInClick()}>Sign Up/Log In</button>
+        {/* {isModalOpen && <SignUpLogIn closeModal={closeModal} />} */}
       </div>
     </nav>
-    )
+  );
 };
