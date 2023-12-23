02e0d7751b1eff8301f1d3800689d8642e5e8c4e
Jeremy  Dutton <jeremyduttonmacbook@Jeremys-MBP.home>
2023-12-22 21:24:02 -0500

    ammended queries/routes to search for books by author, title, and id based on advised by LarryAI chatbot

--- Backend/src/routes/books.js
+++ Backend/src/routes/books.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { query } = require("express");
 const db = require("../database/index.js");
 
 router.get("/books", (req, res) => {
@@ -22,39 +23,64 @@ router.get("/books", (req, res) => {
 
   //returning as raw JSON for resting purposes, see commented
   //possible make it a general object pass to the param
-  const getBookByAuthor = (author) => {
-    return db.query(`SELECT from books
-    WHERE author = books.author`)
+
+  //with assistance from lary AI bot
+  router.get("/books/author", (req, res) => {
+    const authorName = req.body.author; // Get the author's name from the query string
+    getBookByAuthor(authorName);
+  });
+  
+  const getBookByAuthor = (authorName) => {
+    const queryString = `SELECT * FROM books WHERE author = $1`;
+    return db.query(queryString, [author]) // Pass author as a parameter to the query
     .then(({ rows }) => {
       console.log(rows);
       res.json(rows);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
     });
-
   }
 
+  
+  router.get("/books/title", (req, res) => {
+    const bookTitle = req.body.title
+  })
   //returning as raw JSON for testing purposes, see commented
   const getBookByTitle = (title) => {
-    return db.query(`SELECT from books 
-    WHERE author = books.title`)
+    const queryString = `SELECT * FROM books WHERE title =$1`
+    return db.query(queryString, [title])
     .then(({ rows }) => {
       console.log(rows);
       res.json(rows);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
     });
 
   }
 
   //returning as raw JSON for testing purposes
+  router.get("/books:", (req, res) => {
+    const userID = req.body.userID //I don't konw if this is right, but it's what Larry AI recommended
+  })
   const getBooksByUser = (userID) => {
-    return db.query(`SELECT from books 
-    WHERE id = books.id`)
+    const queryString = `SELECT * FROM books WHERE users.id = $1`
+    return db.query(queryString, [userID])
     .then(({ rows }) => {
       console.log(rows);
       res.json(rows);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
     });
 
   }
 
-  
+
 
   // const getBookByAuthor = () => {
   //   return db.query(`SELECT from books
