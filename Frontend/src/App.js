ced9562a8166acd3060a02b20c221f24c97e17eb
lunamoonmoon <5862lunamoon@gmail.com>
2024-01-10 20:44:36 -0800

    api connected to back and front end

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,27 +5,34 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home'
 import Modal from './components/Modal/Modal';
+import Book from './components/Book/Book';
 
 
 function App() {
   const [modalContent, setModalContent] = useState(null);
   const [isSignUp, setIsSignUp] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  let [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const [state, dispatch] = useReducer(searchReducer, initialState);
   const { searchResults } = state;
 
-  const handleSearch = async (query) => {
+  //lets user search term go to backend and fetch from api
+  async function handleSearch() {
+    const searchInput = document.getElementById('searchValue').value;
     try {
-      const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-      dispatch({ type: 'SET_SEARCH_RESULTS', payload: response.data.items });
-      console.log(response.data.items);
-      console.log(searchResults);
-    } catch (error) {
-      console.error(`Error searching for books: ${error.message}`);
+      const res = await fetch(`http://localhost:8001/search?searchTerm=${searchInput}`, {
+      method: 'GET',
+    })
+    if(!res.ok) {
+      throw new Error(`Error: ${res.status}`);
     }
-  };
+    const data = await res.json();
+      dispatch({ type: 'SET_SEARCH_RESULTS', payload: data });
+    } catch(err) {
+      console.error(`Error fetching books: ${err}`)
+    }
+  }
 
   const openModal = (content) => {
     setModalContent(content);
@@ -40,6 +47,7 @@ function App() {
   return (
     <div className="App">
       <NavBar isLoggedIn={true} openModal={openModal} handleSearch={handleSearch}/>
+      < Book searchResults={searchResults} />
       <Home openModal={openModal} searchResults={searchResults} />
       {isModalOpen && modalContent && (
         <Modal closeModal={closeModal} title={modalContent.type.name} body={modalContent} />
