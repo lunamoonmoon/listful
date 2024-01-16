c0debb9e58ded3845aa52a91e04ca90ad1a04fd7
@jerscape <jeremydutton5@gmail.com>
2024-01-16 10:44:26 -0500

    created filter reducer hook

---
+++ Frontend/src/hooks/filterReducer.js
@@ -0,0 +1,16 @@
+const initialFilterState = {
+  filterResults: [],
+};
+
+const bookReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_FILTER_RESULTS':
+      return{ ...state, filterResults: action.payload.filterResults.items };
+    case 'CLEAR_BOOK_RESULTS':
+      return { ...state, filterResults: []};
+    default:
+      return state;
+  }
+};
+
+export { initialFilterState, filterReducer }
